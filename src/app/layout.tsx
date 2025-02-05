51f3a67a979b1257c737fd5d8986c0c3bc98c0f6
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-05 16:38:14 +0700

    feat: add favicon and logo assets for light and dark themes; update layout metadata for icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,46 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_APP_NAME,
   description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+  openGraph: {
+    title: process.env.NEXT_PUBLIC_APP_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    images: [
+      {
+        url: "/assets/favicon/dark/android-chrome-512x512.png",
+        width: 512,
+        height: 512,
+      },
+      {
+        url: "/assets/favicon/light/android-chrome-512x512.png",
+        width: 512,
+        height: 512,
+      },
+    ],
+  },
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/assets/favicon/light/favicon.ico",
+        href: "/assets/favicon/light/favicon.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/assets/favicon/dark/favicon.ico",
+        href: "/assets/favicon/dark/favicon.ico",
+      },
+    ],
+  },
 };
 
 type RootLayoutProps = {
@@ -15,7 +51,9 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-slate-100 dark:bg-background`}>
+      <body
+        className={`${roboto.className} antialiased bg-slate-100 dark:bg-background`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: process.env.NEXT_PUBLIC_APP_NAME,
+    short_name: process.env.NEXT_PUBLIC_APP_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/assets/favicon/light/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/assets/favicon/light/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  };
+}
