78040a427dc2c72954d5900e59d3c6a6011a244e
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-07 10:30:42 +0700

    feat: add additional origin to whitelist in middleware for CORS support

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,6 +1,9 @@
 import { notAllowed } from "./api/config/limiter";
 
-export const whitelist = [process.env.NEXT_PUBLIC_APP_URL];
+export const whitelist = [
+  process.env.NEXT_PUBLIC_APP_URL,
+  "https://visionalyze-git-feat-add-cors-to-middleware-fachryafrz-projects.vercel.app",
+];
 
 export function middleware(request: Request) {
   const origin = request.headers.get("origin");
