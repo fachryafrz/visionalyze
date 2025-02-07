13c66dd6fa6efbac3c12e48cff9e9f1d8fdc37cf
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-07 09:17:38 +0700

    feat: implement CORS middleware and update image analysis API integration

---
+++ src/app/api/analyze/route.ts
@@ -0,0 +1,26 @@
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { image } = await request.json();
+
+  const { text } = await generateText({
+    model: google("gemini-2.0-flash-lite-preview-02-05"),
+    system: `You are a helpful assistant. Respond to the user in Markdown format.`,
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: `Analyze the image and provide a plain JSON response with no markdown formatting, no backslashes or escapes (like \\n). Use this exact JSON format and ensure the output is a valid JSON object:\n\n{\n  "image_information": "Identify the image and provide its name.",\n  "important_information": "Description of the content, objects, details of the image, and key insights or important elements from the image.",\n  "related_keywords": ["Keyword1", "Keyword2", "Keyword3"],\n  "related_questions": ["Question 1?", "Question 2?", "Question 3?"]\n}\n\nEnsure that the output is raw JSON without any escape characters or extra formatting.`,
+          },
+          { type: "image", image },
+        ],
+      },
+    ],
+  });
+
+  return NextResponse.json({ text });
+}
--- src/components/image-upload.tsx
+++ src/components/image-upload.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { anaylyze } from "@/server/actions";
+import axios from "axios";
 import { useFile } from "@/zustand/file";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
@@ -115,9 +115,11 @@ export default function ImageUpload() {
 
     setLoading(true);
 
-    const { text } = await anaylyze(base64IMG);
+    // const { text } = await anaylyze(base64IMG);
 
-    setAnalyze(text);
+    const { data } = await axios.post("/api/analyze", { image: base64IMG });
+
+    setAnalyze(data.text);
 
     setLoading(false);
   };
@@ -253,7 +255,7 @@ function AnalyzeButton({
   handleClear,
 }: AnalyzeButtonProps) {
   return (
-    <>
+    <div className={`flex items-center gap-1 flex-1 w-full sm:flex-grow-0`}>
       {image && (
         <Button
           variant={`ghost`}
@@ -275,6 +277,6 @@ function AnalyzeButton({
       >
         {loading ? <LoadingSpinner /> : <span>Analyze Image</span>}
       </Button>
-    </>
+    </div>
   );
 }
