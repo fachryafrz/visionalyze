11c5a5b6c6b472c7c9439224e59f9ee70fa22913
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-07 10:53:08 +0700

    feat: update CORS whitelist to include new preview URL

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,7 +2,7 @@ import { notAllowed } from "./api/config/limiter";
 
 export const whitelist = [
   process.env.NEXT_PUBLIC_APP_URL,
-  "https://visionalyze-git-feat-add-cors-to-middleware-fachryafrz-projects.vercel.app",
+  "https://preview-visionalyze.vercel.app",
 ];
 
 export function middleware(request: Request) {
