ab3c5f865d45fe972f72ca24cc84652e0003d096
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-26 10:35:05 +0700

    chore: update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Visionalyze",
   description:
-    "Visionalyze is an AI-powered image analysis tool that helps you extract meaningful insights from images effortlessly. Whether you're analyzing photos for content, color composition, or object recognition, Visionalyze provides detailed breakdowns with just one upload!",
+    "AI-powered image analysis tool that helps you extract meaningful insights from images effortlessly. Whether you're analyzing photos for content, color composition, or object recognition, Visionalyze provides detailed breakdowns with just one upload!",
   url:
     process.env.NODE_ENV === "production"
       ? "https://visionalyze.fachryafrz.com"
